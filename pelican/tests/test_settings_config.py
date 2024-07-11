d54de94d2ab6cfa11dd7ede31fad9b50abac66e1
egberts <egberts@github.com>
2024-07-11 13:11:28 -0500

    Move deprecated test cases to its own dedicated test_settings_deprecated.py (from test_settings.py).

--- pelican/tests/test_settings_config.py
+++ pelican/tests/test_settings_config.py
@@ -2,8 +2,10 @@ import unittest
 
 
 class TestSettingsConfig(unittest.TestCase):
+    """Exercise the configure_settings()/settings.py"""
+
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == "__main__":
