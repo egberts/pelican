93b1f837d5d6f863d0e05bca1dcd2f6be9645d1a
egberts <egberts@github.com>
2024-08-02 17:50:37 -0500

    improve test_settings_canon.py

--- pelican/tests/test_settings_canon.py
+++ pelican/tests/test_settings_canon.py
@@ -27,7 +27,7 @@ class TestSettingsCanon:
     def test_reject_last_digit_pass(self):
         bad_module_name = "paramount2"
 
-        assert False is validate_module_name(bad_module_name)
+        assert validate_module_name(bad_module_name) is False
 
     def test_reject_not_identifier_pass(self):
         bad_module_name = "bad-module"
@@ -37,35 +37,39 @@ class TestSettingsCanon:
     def test_reject_multiple_underscores_pass(self):
         bad_module_name = "bad__module"
 
-        assert False is validate_module_name(bad_module_name)
+        assert validate_module_name(bad_module_name) is False
 
 
 class TestSettingsCanonNormalize:
     """canonicalization_module_name()"""
 
     def test_normalize_dash(self):
+        """Replace all dash into underscore"""
         funky_modname = "zippy-ity-doo-dah"
         expected_modname = "zippy_ity_doo_dah"
 
-        assert expected_modname == canonicalize_module_name(funky_modname)
+        assert canonicalize_module_name(funky_modname) == expected_modname
 
     def test_normalize_period(self):
+        """Replace any and all period into an underscore"""
         funky_modname = "how.deep.the.rabbit.hole.goes"
         expected_modname = "how_deep_the_rabbit_hole_goes"
 
-        assert expected_modname == canonicalize_module_name(funky_modname)
+        assert canonicalize_module_name(funky_modname) == expected_modname
 
-    def test_normalize_lowercase(self):
+    def test_normalize_double_underscore(self):
+        """Reduce double underscore symbols into a single underscore"""
         funky_modname = "snake_case__makes__a_____great_case"
         expected_modname = "snake_case_makes_a_great_case"
 
-        assert expected_modname == canonicalize_module_name(funky_modname)
+        assert canonicalize_module_name(funky_modname) == expected_modname
 
-    def test_normalize_double_underscore(self):
+    def test_normalize_lowercase(self):
+        """Force to all lower-case"""
         funky_modname = "SnAke_CaSe_MaKeS_a_gReaT_casE"
         expected_modname = "snake_case_makes_a_great_case"
 
-        assert expected_modname == canonicalize_module_name(funky_modname)
+        assert canonicalize_module_name(funky_modname) == expected_modname
 
 
 # Python: Minimum required versions: 2.3, 3.0  (vermin v1.6.0)
